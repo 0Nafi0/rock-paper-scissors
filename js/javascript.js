fae88c4f4964a3e3ac8b79864f22641492225a2d
Nafi <128926697+0Nafi0@users.noreply.github.com>
2023-09-30 00:40:26 +0600

    Addition of DOM interaction without CSS styling

--- js/javascript.js
+++ js/javascript.js
@@ -1,34 +1,58 @@
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
-    let random = Math.floor(Math.random() * choices.length);
+    const choices = ["Rock", "Paper", "Scissors"];
+    const random = Math.floor(Math.random() * choices.length);
     return choices[random];
 }
 
 function titleCase(string) {
-    new_string = "";
-    new_string += string.charAt(0).toUpperCase();
-    new_string += string.slice(1).toLowerCase();
+    const new_string = string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
     return new_string;
 }
 
-function playRound(playerChoice, computerChoice) {
+const body = document.querySelector("body");
+const scoreboard = document.querySelector(".scoreboard");
+let playerScore = 0;
+let botScore = 0;
+
+function playRound(playerChoice) {
+    playerChoice = titleCase(playerChoice);
+    const computerChoice = getComputerChoice();
+    const roundVerdict = document.createElement("div");
+    roundVerdict.setAttribute("class", "round-result");
+    roundVerdict.textContent = "You chose " + playerChoice + " while the BOT chose " + computerChoice;
+    const score = document.createElement("div");
     if (win_condition[playerChoice] == computerChoice)
-    return "You win! " + playerChoice + " beats " + computerChoice;
-    else {
-        if (playerChoice == computerChoice)
-        return "Draw! Both players chose " + playerChoice;
-        else
-        return "You lose! You chose " + playerChoice + " while Computer chose " + computerChoice; 
+    {
+        playerScore++;
     }
+    else
+    {
+        if (playerChoice == computerChoice)
+        {
+            roundVerdict.textContent = roundVerdict.textContent + ". Draw!";
+        }
+        else
+        {
+            roundVerdict.textContent = roundVerdict.textContent;
+            botScore++;
+        }
+    }
+    score.textContent = "Your Score: " + playerScore + "    " + "BOT Score: " + botScore;
+    scoreboard.appendChild(score);
+    scoreboard.removeChild(score.previousSibling);
+    body.appendChild(roundVerdict);
+    body.removeChild(roundVerdict.previousSibling);
 }
 
-
-let userChoice = titleCase(prompt("Rock, Paper or Scissors?"));
-let computerChoice = getComputerChoice();
 let win_condition = {
     "Paper": "Rock",
     "Rock": "Scissors",
     "Scissors": "Paper",
 };
 
-console.log(playRound(userChoice, computerChoice));
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.className);
+    })
+})
